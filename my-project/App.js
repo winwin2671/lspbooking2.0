59f012e8fa302ac7fc5fe202c298ea6478972b09
winwin2671 <88149505+winwin2671@users.noreply.github.com>
2023-09-26 21:02:31 +0700

    change local db to supabase

--- my-project/App.js
+++ my-project/App.js
@@ -9,135 +9,120 @@ import {
 } from 'react-native'
 import { ListItem } from 'react-native-elements'
 import { Picker } from '@react-native-picker/picker'
-import SQLite from 'react-native-sqlite-storage'
 
-// const db = SQLite.openDatabase(
-//   {
-//     name: 'database',
-//     location: 'default',
-//   },
-//   () => {
-//     // Database opened successfully
-//     console.log('Database opened ')
-//   },
-//   // (error) => {
-//   //   console.error('Error opening database:', error)
-//   // },
-// )
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  'https://cjvslcpxzkwjnrfvfmdn.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqdnNsY3B4emt3am5yZnZmbWRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ4MzI0NTIsImV4cCI6MjAxMDQwODQ1Mn0._zfFCngW8vJytf3n6NyV2NcWpUQ5egDmNSr1cOQd4sg',
+)
 
 const App = () => {
-  //bug here
-
   const [Floor, setFloor] = useState('')
   const [selectedDate, setselectedDate] = useState('')
   const [Period, setPeriod] = useState('')
   const [purpose, setPurpose] = useState('')
   const [results, setResults] = useState([])
+  const [roomdb, setRoom] = useState([])
 
-  const handleSearch = () => {
-    // Construct the SQL query based on the selected values
-    const query = `
-      SELECT room
-      FROM freeclass
-      WHERE floor = ?
-    `
+  const handleSearch = async () => {
+    // Construct the query based on selected criteria
+    const query = supabase.from('roomdb').select('room').eq('floor', Floor) // Filter by selected floor
 
-    // Execute the query with parameters
-    db.transaction((tx) => {
-      tx.executeSql(
-        query,
-        [Floor],
-        (tx, results) => {
-          // Extract and set the results to the component's state
-          const rows = results.rows
-          const data = []
-          for (let i = 0; i < rows.length; i++) {
-            data.push(rows.item(i))
-          }
-          setResults(data)
-        },
-        (error) => {
-          console.error('Error querying database:', error)
-        },
-      )
-    })
+    try {
+      // Execute the query
+      const { data } = await query
+
+      // Update the results state with the filtered room data
+      setResults(data)
+    } catch (error) {
+      // Handle any errors, e.g., show an error message
+      console.error('Error searching for rooms:', error)
+    }
   }
-
   return (
-    <View style={styles.container}>
-      <Picker
-        style={styles.input}
-        selectedValue={Floor}
-        onValueChange={(itemValue, itemIndex) => setFloor(itemValue)}
-      >
-        <Picker.Item label="Floor" />
-        <Picker.Item label="Floor 1" value="1" />
-        <Picker.Item label="Floor 2" value="2" />
-        <Picker.Item label="Floor 3" value="3" />
-        <Picker.Item label="Floor 4" value="4" />
-        <Picker.Item label="Floor 5" value="5" />
-      </Picker>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Picker
+          style={styles.input}
+          selectedValue={Floor}
+          onValueChange={(itemValue, itemIndex) => setFloor(itemValue)}
+        >
+          <Picker.Item label="Floor" />
+          <Picker.Item label="Floor 1" value="1" />
+          <Picker.Item label="Floor 2" value="2" />
+          <Picker.Item label="Floor 3" value="3" />
+          <Picker.Item label="Floor 4" value="4" />
+          <Picker.Item label="Floor 5" value="5" />
+        </Picker>
 
-      <Picker
-        style={styles.input}
-        selectedValue={selectedDate}
-        onValueChange={(itemValue, itemIndex) => setselectedDate(itemValue)}
-      >
-        <Picker.Item label="Day" />
-        <Picker.Item label="Monday" value="Mon" />
-        <Picker.Item label="Tuesday" value="Tue" />
-        <Picker.Item label="Wednesday" value="Wen" />
-        <Picker.Item label="Thursday" value="Thu" />
-        <Picker.Item label="Friday" value="Fri" />
-      </Picker>
+        <Picker
+          style={styles.input}
+          selectedValue={selectedDate}
+          onValueChange={(itemValue, itemIndex) => setselectedDate(itemValue)}
+        >
+          <Picker.Item label="Day" />
+          <Picker.Item label="Monday" value="Mon" />
+          <Picker.Item label="Tuesday" value="Tue" />
+          <Picker.Item label="Wednesday" value="Wen" />
+          <Picker.Item label="Thursday" value="Thu" />
+          <Picker.Item label="Friday" value="Fri" />
+        </Picker>
 
-      <Picker
-        style={styles.input}
-        selectedValue={Period}
-        onValueChange={(itemValue, itemIndex) => setPeriod(itemValue)}
-      >
-        <Picker.Item label="Period" />
-        <Picker.Item label="Period 1" value="1" />
-        <Picker.Item label="Period 2" value="2" />
-        <Picker.Item label="Period 3" value="3" />
-        <Picker.Item label="Period 4" value="4" />
-        <Picker.Item label="Period 5" value="5" />
-        <Picker.Item label="Period 6" value="6" />
-        <Picker.Item label="Period 7" value="7" />
-        <Picker.Item label="Period 8" value="8" />
-        <Picker.Item label="Period 9" value="9" />
-      </Picker>
+        <Picker
+          style={styles.input}
+          selectedValue={Period}
+          onValueChange={(itemValue, itemIndex) => setPeriod(itemValue)}
+        >
+          <Picker.Item label="Period" />
+          <Picker.Item label="Period 1" value="1" />
+          <Picker.Item label="Period 2" value="2" />
+          <Picker.Item label="Period 3" value="3" />
+          <Picker.Item label="Period 4" value="4" />
+          <Picker.Item label="Period 5" value="5" />
+          <Picker.Item label="Period 6" value="6" />
+          <Picker.Item label="Period 7" value="7" />
+          <Picker.Item label="Period 8" value="8" />
+          <Picker.Item label="Period 9" value="9" />
+        </Picker>
 
-      <Picker
-        style={styles.input}
-        selectedValue={purpose}
-        onValueChange={(itemValue, itemIndex) => setPurpose(itemValue)}
-      >
-        <Picker.Item label="Select your Purpose" />
-        <Picker.Item label="Meeting" />
-        <Picker.Item label="Group work" />
-        <Picker.Item label="Lab" />
-        <Picker.Item label="Music" />
-        <Picker.Item label="Other" />
-      </Picker>
+        <Picker
+          style={styles.input}
+          selectedValue={purpose}
+          onValueChange={(itemValue, itemIndex) => setPurpose(itemValue)}
+        >
+          <Picker.Item label="Select your Purpose" />
+          <Picker.Item label="Meeting" />
+          <Picker.Item label="Group work" />
+          <Picker.Item label="Lab" />
+          <Picker.Item label="Music" />
+          <Picker.Item label="Other" />
+        </Picker>
 
-      <Button title="Search" onPress={handleSearch} />
+        <Button title="Search" onPress={handleSearch} />
 
-      <FlatList
-        data={results}
-        keyExtractor={(item) => (item ? item.room.toString() : '')} // Ensure 'room' is unique
-        renderItem={({ item }) => (
-          <ListItem bottomDivider>
-            <ListItem.Content>
-              <ListItem.Title>{item ? item.room : 'No data'}</ListItem.Title>
-            </ListItem.Content>
-          </ListItem>
+        {results.length === 0 ? (
+          <Text>No results found</Text>
+        ) : (
+          <FlatList
+            data={results}
+            keyExtractor={(item) => (item ? item.room.toString() : '')}
+            renderItem={({ item }) => (
+              <ListItem bottomDivider>
+                <ListItem.Content>
+                  <ListItem.Title>
+                    {item ? item.room : 'No data'}
+                  </ListItem.Title>
+                </ListItem.Content>
+              </ListItem>
+            )}
+          />
         )}
-      />
-    </View>
+      </View>
+      <Text>LSP room finder</Text>
+    </SafeAreaView>
   )
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
