db1af0393df4cfb9cde8dc21f5c46cb4633f4198
winwin2671 <88149505+winwin2671@users.noreply.github.com>
2023-11-01 17:52:54 +0700

    update login

--- my-project/login.js
+++ my-project/login.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { View, Text, Button } from 'react-native'
-import { SupabaseClient } from '@supabase/supabase-js' // Import your SupabaseClient
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  'https://cjvslcpxzkwjnrfvfmdn.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqdnNsY3B4emt3am5yZnZmbWRuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NDgzMjQ1MiwiZXhwIjoyMDEwNDA4NDUyfQ.PJGJyRmABF9ifz4oxp5v-CSonFywIY4mrhcBnznNkjA',
+)
 
 function Login({ navigation }) {
   async function loginWithGoogle() {
@@ -16,7 +21,7 @@ function Login({ navigation }) {
   return (
     <View>
       <Text>Login Page</Text>
-
+      {/* Implement your login UI elements, buttons, and functionality */}
       <Button title="Login with Google" onPress={loginWithGoogle} />
     </View>
   )
