6dba29af3e8d21b4c19ec9bf8c3dc16725efc895
winwin2671 <88149505+winwin2671@users.noreply.github.com>
2023-11-08 11:07:50 +0700

    delete text

--- my-project/App.js
+++ my-project/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { Text } from 'react-native'
 import Login from './login'
 import MainApp from './MainApp'
 
