32bf51cefb6db050b085a2a0923e2a4aa7909c9d
winwin2671 <88149505+winwin2671@users.noreply.github.com>
2023-11-16 15:09:30 +0700

    remove supabase createclient

--- my-project/login.js
+++ my-project/login.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react'
 import { View, Text, Button, SafeAreaView, StyleSheet } from 'react-native'
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  'https://cjvslcpxzkwjnrfvfmdn.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqdnNsY3B4emt3am5yZnZmbWRuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NDgzMjQ1MiwiZXhwIjoyMDEwNDA4NDUyfQ.PJGJyRmABF9ifz4oxp5v-CSonFywIY4mrhcBnznNkjA',
-)
+// const supabase = createClient(
+//   'https://cjvslcpxzkwjnrfvfmdn.supabase.co',
+//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqdnNsY3B4emt3am5yZnZmbWRuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NDgzMjQ1MiwiZXhwIjoyMDEwNDA4NDUyfQ.PJGJyRmABF9ifz4oxp5v-CSonFywIY4mrhcBnznNkjA',
+// )
 
 function Login({ navigation }) {
   async function loginWithGoogle() {
